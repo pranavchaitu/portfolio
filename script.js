377686a729f1dff1ba68b52a522a62d8ba374f53
Pranav <pranavchaitu676@gmail.com>
2023-11-07 16:16:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const navLinks = document.querySelectorAll("nav a");
+navLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+    const targetId = link.getAttribute("href").substring(1);
+    const targetSection = document.getElementById(targetId);
+    window.scrollTo({
+      top: targetSection.offsetTop,
+      behavior: "smooth",
+    });
+  });
+});
