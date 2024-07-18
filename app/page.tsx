4fb0280acc11f27d0ec8b0450f95a012c6d4e961
pranavchaitu <pranavchaitu676@gmail.com>
2024-07-18 19:25:26 +0530

    mod sizes

--- app/page.tsx
+++ app/page.tsx
@@ -18,27 +18,27 @@ export default function Home() {
             <p className="font-bold text-4xl lg:text-5xl mt-6 sm:mt-5">
             Programmer. Student. Sometimes Chess Player.
             </p>
-            <p className="text-lg sm:text-base text-zinc-600 mt-6 sm:mt-5">
+            <p className="text-base text-zinc-600 mt-6 sm:mt-5">
               Hey, I&apos;m Pranav. I&apos;m a <span className="text-gray-800 font-bold">TypeScript Engineer</span> focused on efficient application development. I&apos;m passionate about coding and constantly learning to improve.
             </p>
-            <p className="text-lg sm:text-base text-zinc-600 mt-5">
+            <p className="text-base text-zinc-600 mt-5">
               Previously, I developed innovative web solutions and applications. I also created projects like a <a href="https://bloggitt.vercel.app/" target="_blank" className="text-gray-800 font-bold underline cursor-pointer">Blogging Site</a>
               , <a href="https://paytm-by-pranav.vercel.app/" target="_blank" className="text-gray-800 font-bold underline cursor-pointer">Wallet/Transaction App</a>
               , and <a href="https://dukaan-by-pranav.vercel.app/" target="_blank" className="text-gray-800 font-bold underline cursor-pointer">Dashboard of Dukaan.</a>
             </p>  
             <div className="flex gap-6 mt-6 sm:mt-5">
               <a href="https://www.x.com/pranavchaitu" target="_blank">
-                <div className="w-8 sm:w-6">
+                <div className="w-6">
                   <TwitterIcon/>
                 </div>
               </a>
               <a href="https://www.github.com/pranavchaitu" target="_blank">
-              <div className="w-8 sm:w-6">
+              <div className="w-6">
                   <GitHubIcon/>
                 </div>
               </a>              
               <a href="https://www.linkedin.com/in/pranav-repuri-342878259/" target="_blank">
-                <div className="w-8 sm:w-6">
+                <div className="w-6">
                   <LinkedInIcon/>
                 </div>
               </a>              
