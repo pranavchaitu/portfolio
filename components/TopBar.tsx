6d5a201a0abfce75dc5046c307cd4bbaf66856e1
pranavchaitu <pranavchaitu676@gmail.com>
2024-07-18 19:03:20 +0530

    useCallback mod

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { MenuBar } from "./MenuBar";
 import { NavBar } from "./NavBar";
 
@@ -8,30 +8,30 @@ export function TopBar() {
     const [show, setShow] = useState(true);
     const [lastScrollY, setLastScrollY] = useState(0);
 
-    const controlNavbar = () => {
+    const controlNavbar = useCallback(() => {
         if (typeof window !== 'undefined') {
-        if (window.scrollY > lastScrollY) {
-            // if scrolling down, hide the navbar
-            setShow(false);
-        } else {
-            // if scrolling up, show the navbar
-            setShow(true);
-        }
+            if (window.scrollY > lastScrollY) {
+                // if scrolling down, hide the navbar
+                setShow(false);
+            } else {
+                // if scrolling up, show the navbar
+                setShow(true);
+            }
 
-        // remember the current page location for the next move
-        setLastScrollY(window.scrollY);
+            // remember the current page location for the next move
+            setLastScrollY(window.scrollY);
         }
-    };
+    },[lastScrollY]);
 
     useEffect(() => {
         if (typeof window !== 'undefined') {
-          window.addEventListener('scroll', controlNavbar);
-          // cleanup function
-          return () => {
-            window.removeEventListener('scroll', controlNavbar);
-          };
+            window.addEventListener('scroll', controlNavbar);
+            // cleanup function
+            return () => {
+                window.removeEventListener('scroll', controlNavbar);
+            };
         }
-      }, [setLastScrollY]);
+    }, [controlNavbar]);
 
     return <>
         <div className={`transition-transform duration-300 transform ${show ? 'translate-y-0' : '-translate-y-full'}`}>
