894e4c402fb0b13dcecc5e229778ba289559c1d0
pranavchaitu <pranavchaitu676@gmail.com>
2024-07-18 20:54:52 +0530

    home modX

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
               Hey, I&apos;m Pranav, a third-year <span className="text-md text-gray-800 font-bold">CSE student</span> passionate about <span className="text-md text-gray-800 font-bold">TypeScript development</span>. I focus on efficient application development and continuously enhance my coding skills.
             </p>
             <p className="text-base text-zinc-600 mt-5">
-              Previously, I've built projects like a <a href="https://medium-by-pranav.vercel.app" target="_blank" className="text-gray-800 font-bold underline cursor-pointer">Blogging Site</a>, <a href="https://paytm-by-pranav.vercel.app" target="_blank" className="text-gray-800 font-bold underline cursor-pointer">Wallet/Transaction App</a>, and <a href="https://dukaan-by-pranav.vercel.app" target="_blank" className="text-gray-800 font-bold underline cursor-pointer">Dukaan Dashboard</a>.
+              Previously, I&apos;ve built projects like a <a href="https://medium-by-pranav.vercel.app" target="_blank" className="text-gray-800 font-bold underline cursor-pointer">Blogging Site</a>, <a href="https://paytm-by-pranav.vercel.app" target="_blank" className="text-gray-800 font-bold underline cursor-pointer">Wallet/Transaction App</a>, and <a href="https://dukaan-by-pranav.vercel.app" target="_blank" className="text-gray-800 font-bold underline cursor-pointer">Dukaan Dashboard</a>.
             </p>  
             <div className="flex gap-6 mt-6 sm:mt-5">
               <a href="https://www.x.com/pranavchaitu" target="_blank">
