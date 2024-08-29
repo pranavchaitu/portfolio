11954649cf25192baed2891bb9f6c3788d462c60
Pranav <pranavchaitu676@gmail.com>
2024-08-29 23:58:44 +0530

    Update page.tsx

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,7 +1,7 @@
 export default function Resume() {
     return <div className="m-6 lg:m-0 flex justify-start">
         <iframe 
-            src="/self/resume.pdf"
+            src="https://drive.google.com/file/d/1lHw-Bam2jFmOc7KvlSY1QdlipipUVATA/view"
             className="w-screen h-screen max-w-screen-lg rounded-lg"
         />
     </div>     
